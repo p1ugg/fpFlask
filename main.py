9a5bf7dcda69c372cfe42159816a98dc87f81750
p1ugg <67374872+p1ugg@users.noreply.github.com>
2023-02-11 18:40:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, request
 
 app = Flask(__name__)
 
@@ -51,6 +51,58 @@ def image_mars():
                       Присоединяйся!
                     </div>
                   </body>
+                </html>"""
+
+
+@app.route('/carousel')
+def carousel():
+    return f"""<!DOCTYPE html>
+                <html lang="en">
+                <head>
+                  <title>пейзажи йопиресете</title>
+                  <meta charset="utf-8">
+                  <meta name="viewport" content="width=device-width, initial-scale=1">
+                  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+                  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+                  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+                </head>
+                <body>
+                <div class="container">
+                  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                    <!-- Indicators -->
+                    <ol class="carousel-indicators">
+                      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                      <li data-target="#myCarousel" data-slide-to="1"></li>
+                      <li data-target="#myCarousel" data-slide-to="2"></li>
+                    </ol>
+                
+                   
+                    <div class="carousel-inner">
+                      <div class="item active">
+                        <img src="{url_for('static', filename='img/first.jpg')}" alt="1" style="width:100%;">
+                      </div>
+                
+                      <div class="item">
+                        <img src="{url_for('static', filename='img/second.jpg')}" alt="2" style="width:100%;">
+                      </div>
+                    
+                      <div class="item">
+                        <img src="{url_for('static', filename='img/tri.jpg')}" alt="3" style="width:100%;">
+                      </div>
+                    </div>
+                
+                    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                      <span class="glyphicon glyphicon-chevron-left"></span>
+                      <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                      <span class="glyphicon glyphicon-chevron-right"></span>
+                      <span class="sr-only">Next</span>
+                    </a>
+                  </div>
+                </div>
+                
+                </body>
                 </html>"""
 
 
